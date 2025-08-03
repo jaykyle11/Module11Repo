dd93c545e813d13553bc1c076aaf2eacfeb86c62
jaykyle11 <jfontenot11@gmail.com>
2025-08-03 17:31:20 -0400

    Added a new message

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-# Firstname Lastname
-# 11/15/12024
+# Justin Fontenot
+# 8/3/2025
 # A simple Python program to print a message
 
+# Justin Fontenot
+# 8/3/2025
+#added new message
+
 print("Hello, World!")
+
+print("Welcome to GitHub!")
